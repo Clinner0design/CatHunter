9c974af57873ae83993984596e03328b2dee9c58
Clinner0design <113261237+Clinner0design@users.noreply.github.com>
2023-09-25 19:38:31 +0700

    Add sketch file for esp32

---
+++ project.ino
@@ -0,0 +1,83 @@
+/*
+   -- New project --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 3.1.8 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.11.1 or later version;
+     - for iOS 1.9.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// определение режима соединения и подключение библиотеки RemoteXY 
+#define REMOTEXY_MODE__ESP32CORE_BLUETOOTH
+#include <BluetoothSerial.h>
+
+#include <RemoteXY.h>
+
+// настройки соединения 
+#define REMOTEXY_BLUETOOTH_NAME "Cat Hunter"
+
+
+// конфигурация интерфейса  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 47 bytes
+  { 255,1,0,0,0,40,0,16,174,1,1,0,20,39,25,25,1,2,208,158,
+  208,162,208,159,208,163,208,147,208,157,208,163,208,162,208,172,32,208,154,208,
+  158,208,162,208,144,33,0 };
+  
+// структура определяет все переменные и события вашего интерфейса управления 
+struct {
+
+    // input variables
+  uint8_t button_1; // =1 если кнопка нажата, иначе =0 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+   
+
+  
+  
+  // TODO you loop code
+  // используйте структуру RemoteXY для передачи данных
+  // не используйте функцию delay() 
+  if (RemoteXY.button_1 == 1) {
+  digitalWrite(5, HIGH); // подать питание на пин D6
+  } else {
+  digitalWrite(5, LOW); // снять питание с пина D6
+}
+
+
+}
